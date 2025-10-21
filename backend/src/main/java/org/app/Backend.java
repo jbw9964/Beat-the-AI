c2deb2eb1f0ce39b8342f31ca9b32eb7542d3fac
청주는사과아님 <jbw9964@gmail.com>
2025-10-22 00:37:06 +0900

    init backend

---
+++ backend/src/main/java/org/app/Backend.java
@@ -0,0 +1,13 @@
+package org.app;
+
+import org.springframework.boot.*;
+import org.springframework.modulith.*;
+
+@Modulith
+public class Backend {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backend.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/app/BackendTests.java
@@ -0,0 +1,14 @@
+package org.app;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.*;
+
+@SpringBootTest
+class BackendTests {
+
+    @Test
+    @DisplayName("컨텍스트 로드")
+    void contextLoads() {
+    }
+
+}
