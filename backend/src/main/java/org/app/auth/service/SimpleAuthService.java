1ca986e3afb088ef508797a0566a827da14291ff
청주는사과아님 <jbw9964@gmail.com>
2025-11-02 01:29:47 +0900

    enhance: 회원가입 동시성 문제 개선

--- backend/src/main/java/org/app/auth/service/SimpleAuthService.java
+++ backend/src/main/java/org/app/auth/service/SimpleAuthService.java
@@ -1,15 +1,18 @@
 package org.app.auth.service;
 
 import lombok.*;
+import lombok.extern.slf4j.*;
 import org.app.auth.domain.exception.*;
 import org.app.auth.dto.*;
 import org.app.auth.repository.*;
 import org.app.entity.*;
 import org.app.util.exception.*;
+import org.springframework.dao.*;
 import org.springframework.security.crypto.password.*;
 import org.springframework.stereotype.*;
 import org.springframework.transaction.annotation.*;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SimpleAuthService {
@@ -33,16 +36,18 @@ public class SimpleAuthService {
     @Transactional
     public Long idPwSignup(String username, String loginId, String password, String email) {
 
-        if (userRepo.findByLoginId(loginId).isPresent()) {
-            throw new DuplicateLoginIDException();
-        }
-
         String encryptedPw = pwEncoder.encode(password);
         User newUser = new User(username, loginId, encryptedPw);
         newUser.changeEmail(email);
-        userRepo.save(newUser);
 
-        return newUser.getId();
+        try {
+            // 확실히 동시성 관련한 고민거리 대부분을 없애주지만
+            // 정말 down-side 는 없을지 궁금하네..
+            return userRepo.save(newUser).getId();
+        } catch (DataIntegrityViolationException e) {
+            log.warn("Failed to save entity: {}", e.getMessage(), e);
+            throw new DuplicateLoginIDException();
+        }
     }
 
     public Tokens reissueViaRt(String refreshToken) {
