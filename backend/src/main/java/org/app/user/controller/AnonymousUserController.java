32e518538d43bb0721474f8a5c736b09acd35c21
청주는사과아님 <jbw9964@gmail.com>
2025-11-03 17:24:14 +0900

    feat: AnonymouseUserService 관련 endpoint 구성

---
+++ backend/src/main/java/org/app/user/controller/AnonymousUserController.java
@@ -0,0 +1,67 @@
+package org.app.user.controller;
+
+import jakarta.validation.*;
+import lombok.*;
+import lombok.extern.slf4j.*;
+import org.app.user.dto.response.*;
+import org.app.user.service.*;
+import org.app.util.api.*;
+import org.springdoc.core.annotations.*;
+import org.springframework.security.core.annotation.*;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/user/{user_id:\\d+}")
+@RequiredArgsConstructor
+public class AnonymousUserController {
+
+    private final AnonymouseUserService anonymouseUserService;
+
+    // 사용자 정보 보기
+    @GetMapping
+    public ApiResponse<GetUserResponse> getUser(
+            @PathVariable("user_id") Long userId,
+            @AuthenticationPrincipal Long authenticatedUserId
+    ) {
+        GetUserResponse response = anonymouseUserService.getUser(userId, authenticatedUserId);
+
+        return ApiResponse.success(response);
+    }
+
+    // public 한 사용자 플레이 기록 (목록) 보기
+    @GetMapping("/public-record")
+    public ApiResponse<GetPublicRecordsResponse> getPublicRecords(
+            @PathVariable("user_id") Long userId,
+            @Valid @ParameterObject @ModelAttribute
+            SimplePageRequest pageRequest,
+            @AuthenticationPrincipal Long authenticatedUserId
+    ) {
+        // pageRequest :
+        // @ModelAttribute 로 SimplePageRequest 속성 이름 받도록 구성
+        // @ParameterObject 로 swagger 에서 query param 으로 인식하도록 구성
+
+        int pageNo = pageRequest.getPageNumOrDefault();
+        int pageSize = pageRequest.getPageSizeOrDefault();
+
+        GetPublicRecordsResponse response = anonymouseUserService.getPublicRecords(
+                userId, pageNo, pageSize, authenticatedUserId
+        );
+
+        return ApiResponse.success(response);
+    }
+
+    // public 한 사용자 플레이 기록 (단일) 보기
+    @GetMapping("/public-record/{record_id:\\d+}")
+    public ApiResponse<GetPublicRecordResponse> getPublicRecord(
+            @PathVariable("user_id") Long userId,
+            @PathVariable("record_id") Long recordId,
+            @AuthenticationPrincipal Long authenticatedUserId
+    ) {
+        GetPublicRecordResponse response = anonymouseUserService.getPublicRecord(
+                userId, recordId, authenticatedUserId
+        );
+
+        return ApiResponse.success(response);
+    }
+}
--- backend/src/main/java/org/app/util/api/SimplePageRequest.java
+++ backend/src/main/java/org/app/util/api/SimplePageRequest.java
@@ -1,15 +1,26 @@
 package org.app.util.api;
 
+import com.fasterxml.jackson.annotation.*;
 import jakarta.validation.constraints.*;
 
 public record SimplePageRequest(
         @Min(value = 0, message = "페이지 번호는 0 보다 크거나 같아야 합니다")
-        int pageNum,
+        Integer pageNum,
         @Min(value = 1, message = "페이지 크기는 1 보다 크거나 같아야 합니다")
-        int pageSize
+        Integer pageSize
 ) {
 
-    public SimplePageRequest() {
-        this(0, 10);
+    private static final int
+            DEFAULT_PAGE_NUM = 0,
+            DEFAULT_PAGE_SIZE = 10;
+
+    @JsonIgnore
+    public int getPageNumOrDefault() {
+        return pageNum == null ? DEFAULT_PAGE_NUM : pageNum;
+    }
+
+    @JsonIgnore
+    public int getPageSizeOrDefault() {
+        return pageSize == null ? DEFAULT_PAGE_SIZE : pageSize;
     }
 }
